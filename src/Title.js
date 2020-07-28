21c0d8db0f9ce638d4a478f7588eb59be101aed9
Suyun514 <Suyun514@qq.com>
2020-07-29 06:49:05 +0800

    Add Title

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import './Title.css'
+
+function Title() {
+  return (
+    <div className="Title">
+      <header className="Title-header">
+        <h1>OI Calendar</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Title;
