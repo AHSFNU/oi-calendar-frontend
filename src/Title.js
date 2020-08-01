4b74db276ec2118b803506cbbf811c351a519eaa
Suyun514 <Suyun514@qq.com>
2020-08-01 11:26:57 +0800

    Modify title style

--- src/Title.js
+++ src/Title.js
@@ -4,8 +4,8 @@ import { Header, Icon } from 'semantic-ui-react';
 function Title() {
   return (
     <div className="Title">
-      <Header as='h1' textAlign='center'>
-        <Icon name='trophy' circular />
+      <Header as='h1' icon textAlign='center'>
+        <Icon name='calendar' circular />
         OI Calendar
       </Header>
     </div >
